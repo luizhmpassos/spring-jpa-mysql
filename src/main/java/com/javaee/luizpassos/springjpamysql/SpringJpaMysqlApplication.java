26b20c15ca2c72306374e72fb0e9a318529ff6bf
Luiz Henrique Magalhães Passos <1100738@sga.pucminas.br>
2018-07-05 20:23:56 -0300

    submissao inicial

---
+++ src/main/java/com/javaee/luizpassos/springjpamysql/SpringJpaMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.javaee.luizpassos.springjpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaMysqlApplication.class, args);
+	}
+}
